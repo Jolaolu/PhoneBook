436de9df030480fb96fbbe4184a32961a9347273
Jolaolu <adebayoanjola@gmail.com>
2018-06-01 10:52:49 +0100

    Created Database

--- client/main.js
+++ client/main.js
@@ -1,17 +1,17 @@
 import { Template } from 'meteor/templating';
-
+import {Books} from '../lib/collections.js';
 
 import './main.html';
 
 Template.body.helpers({
-    book:[
+   /* phoneBooks:[
         {text: 'My phonebook'},
         {text: 'My phonebook1'},
         {text: 'My phonebook2'},
         {text: 'My phonebook3'}
 
-
-
-
-    ]
+    ]*/
+     phoneBooks() {
+        return Books.find({});
+    }
 })
